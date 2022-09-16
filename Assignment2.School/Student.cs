7943607296703e5ca2c799276ca6adb80cd7fdb1
adamjhr <adam.jh.rose6@gmail.com>
2022-09-16 13:45:54 +0200

    Student passes test

--- Assignment2.School/Student.cs
+++ Assignment2.School/Student.cs
@@ -12,10 +12,15 @@ public class Student {
     }
 
     public int Id { get; init; }
-    public string GivenName { get; set; }
-    public string Surname { get; set; }
+    public string GivenName { get; set; } = null!;
+    public string Surname { get; set; } = null!;
     Status StudentStatus { 
-        get => Status.New;
+        get {
+            if (DateTime.Compare(StartDate, DateTime.Now) >= 0) return Status.New;
+            if (DateTime.Compare(EndDate, DateTime.Now) > 0) return Status.Active;
+            if (DateTime.Compare(EndDate, GraduationDate) < 0) return Status.Dropout;
+            /*if (DateTime.Compare(GraduationDate, DateTime.Now) <= 0)*/ return Status.Graduated;
+        }
         }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
