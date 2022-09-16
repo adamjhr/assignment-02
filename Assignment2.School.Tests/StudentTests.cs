4404d1f4bda8479c3e737d61c7b8dc080c30392a
adamjhr <adam.jh.rose6@gmail.com>
2022-09-16 13:45:37 +0200

    Finalized test

--- Assignment2.School.Tests/StudentTests.cs
+++ Assignment2.School.Tests/StudentTests.cs
@@ -8,11 +8,11 @@ public class UnitTest1
     public void Test1()
     {
 
-        var student = new Student{  Id = 0, GivenName="Peter", Surname="Petersen", StartDate=new DateTime(2020, 05, 09, 9, 15, 00), EndDate=new DateTime(2021, 05, 09, 9, 15, 00), GraduationDate=new DateTime(2019, 05, 09, 9, 15, 00)};
+        var student = new Student{  Id = 0, GivenName="Peter", Surname="Petersen", StartDate=new DateTime(2020, 09, 05, 9, 15, 00), EndDate=new DateTime(2021, 09, 05, 9, 15, 00), GraduationDate=new DateTime(2021, 09, 05, 9, 15, 00)};
 
         var studentString = student.toString();
 
-        studentString.Should().BeEquivalentTo("0 Peter Petersen New 05/09/2020 05/09/2021 05/09/2019");
+        studentString.Should().BeEquivalentTo("0 Peter Petersen Graduated 05-09-2020 05-09-2021 05-09-2021");
 
     }
 }
