4dae44c7b389b5a03c730063333d4151e107194f
adamjhr <adam.jh.rose6@gmail.com>
2022-09-16 15:30:50 +0200

    More extensions

--- Assignment2.Tests/ExtensionsTests.cs
+++ Assignment2.Tests/ExtensionsTests.cs
@@ -13,23 +13,58 @@ public class ExtensionsTests
         result.Should().BeEquivalentTo(new []{1, 2, 4, 5, 99, 130, 23});
     }
 
+    [Fact]
     public void Select_divisible_by_7_less_than_42()
     {
         // HUSK: Spar keystrokes
         var ys = new int[] { 1, 7, 4, 5, 8, 9, 17, 42, 14, 21, 56, 70 };
 
-        var result = ys.Select((num) => num < 42 && num % 7 == 0);
+        var result = ys.Where((num) => num < 42 && num % 7 == 0);
 
         result.Should().BeEquivalentTo(new []{ 7, 14, 21 });
     }
 
+    [Fact]
         public void Select_leap_years()
     {
         // HUSK: Spar keystrokes
         var ys = new int[] { 1200, 4, 1600, 1700, 1704, 99, 32, 41 };
 
-        var result = ys.Select((year) => (year % 400 == 0 || year % 100 != 0) || year % 4 == 0 );
+        var result = ys.Where((year) => (year % 4 == 0 && year % 100 != 0) || year % 400 == 0 );
 
         result.Should().BeEquivalentTo(new []{ 1200, 4, 1600, 1704, 32 });
+    }
+
+    [Fact]
+    public void Url_scheme_is_https() {
+
+        var url = new Uri("https://docs.microsoft.com/en-us/dotnet/api/system.uri.scheme?view=net-6.0");
+
+        var result = url.isSecure();
+
+        result.Should().BeTrue();
+
+    }
+
+    [Fact]
+        public void Url_scheme_is_not_https() {
+
+        var url = new Uri("http://docs.microsoft.com/en-us/dotnet/api/system.uri.scheme?view=net-6.0");
+
+        var result = url.isSecure();
+
+        result.Should().BeFalse();
+
+    }
+
+     [Fact]
+        public void Number_of_words_is_5() {
+
+        var words = "   p   ";
+
+        var result = words.wordCount();
+
+        result.Should().Be(5);
+
     }
 }
